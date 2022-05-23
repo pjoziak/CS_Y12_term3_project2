b180d9bd16b1e976c2faba3cb7798653bf84f237
Paweł Józiak <pjoziak@gmail.com>
2022-05-23 09:47:09 +0200

    revert changes in webserver/rest/wsgi.py

--- webserver/rest/wsgi.py
+++ webserver/rest/wsgi.py
@@ -7,33 +7,18 @@ app = Flask("Lubie placki")
 api = Api(app)
 
 
-class ValidateResource(Resource):
-    def post(self):
-        data = request.get_json(force=True)
-        try:
-            user = data['user']
-            password = data['password']
-            if user == 'a' and password == 'b':
-                return '200', 200
-            else:
-                return '403', 403
-        except KeyError:
-            return '422', 422
-
-
 class MyResource(Resource):
     def post(self):
         print(request.values)
         if request.is_json:
             J = request.get_json()
             print(J)
-            return '', 200
+            return jsonify(J), 200
         else:
             abort(422)
 
 
 api.add_resource(MyResource, '/identity')
-api.add_resource(ValidateResource, '/login')
 
 if __name__ == '__main__':
     app.run(port=PORT)
